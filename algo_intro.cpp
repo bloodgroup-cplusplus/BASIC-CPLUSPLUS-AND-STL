a40a8a1dbae1606f2829ee2049e20ca76e06aee6
cl-rs <81256027+cl-rs@users.noreply.github.com>
2021-06-01 17:47:47 +0530

    Update algo_intro.cpp

--- algo_intro.cpp
+++ algo_intro.cpp
@@ -7,10 +7,10 @@
  * we will be using c++ 11 lambda function
  */
 
-#include<algorithm>
 #include<vector>
 #include<iostream>
 #include<tuple>
+#include<utility>
 
 int main()
 {
@@ -24,10 +24,35 @@ int main()
         std::cout<<"the number of integer value greater than 10 in the vector v is  "<<m<<std::endl;
 
         std::vector<int>::iterator it,it1;
-
-        it=max_element(v.begin()+2,v.end());
-
+        std::pair<std::vector<int>::iterator,std::vector<int>::iterator>mnmx;
+        it=std::max_element(v.begin()+2,v.end());
+        std::cout<<"the maximum element starting from second index to the end of the vector is"<<std::endl;
         std::cout<< *it<<std::endl;
+        it1=std::max_element(v.begin(),v.end(),[](int x, int y){return (x%10)<(y%10);});
+        std::cout<<"This is an example where the iterator is being used to describe our own definition for sorting"<<std::endl;
+        std::cout<<*it1<<std::endl;
+        //Most algorithms have a simple have a simple form and a generalized form
+        it=std::min_element(v.begin(),v.end());
+        std::cout<<"the min element of the given vector is"<<std::endl;
+        std::cout<<*it<<std::endl;
+        mnmx=std::minmax_element(v.begin(),v.end(),[](int x, int y){return (x%10)<(y%10);});
+        //returns a pair, which contains first of min and last of max
+        std::cout<<"the min value is   "<<*mnmx.first<<"the max value is  "<<*mnmx.second<<std::endl;
 
-        return 0;
+        //linear searching (when data is not sorted)
+        //returns first match
+        it=std::find(v.begin(),v.end(),32);
+        std::cout<<*it<<std::endl;
+
+        it=std::find_if(v.begin(),v.end(),[](int x){return x>80;});
+        std::cout<<"the first value in vector greater than 80is  "<<*it<<std::endl;
+        it =std::find_if_not(v.begin(),v.end(),[](int x){return x>80;});
+        std::cout<<"the first value in vector which is not greater than 80 is "<< * it <<std::endl;
+
+        //search for consecitve x times whose value is y
+        //itr=std::search_n(v.begin(),v.end(),x,y);
+        it=std::search_n(v.begin(),v.end(),2,32);
+        std::cout<<"The consecutive  " << 2<< "  elements whose value is 32 is  "<<*it<<std::endl;
+
+
 }
