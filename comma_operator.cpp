86f98255b68e61392005fce849f03f27ccc2c0f6
cl-rs <81256027+cl-rs@users.noreply.github.com>
2021-09-24 21:52:53 +0530

    Create comma_operator.cpp

---
+++ comma_operator.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+int fun()
+{
+        return 1,2,3;
+
+}
+int main()
+{
+        int v1,v2;
+        v1 = 1,2,3;
+
+        std::cout<<v1<<std::endl;
+
+        v2 = (1,2,3);
+
+
+        std::cout<<v2<<std::endl;
+
+        std::cout<<fun()<<std::endl;
+        // comma operator executes from left to right 
+        // so we get the last output while printing 
+        return 0;
+}
